f1eb6a56a5c9ed4efe428fbe1aa55b78493ee103
李晓阳 <2366304062@qq.com>
2016-06-20 14:46:44 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,7 @@
 package com.bluetooth.bluetoothdn;
 
-import java.util.Arrays;
-import java.util.Iterator;
-
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bluetooth.entity.ResultModel;
-import com.bluetooth.impl.IDImpl;
-import com.bluetooth.inter.ID;
-import com.example.sdtverify.sdtVerify;
-import com.fri.bluetoothdn.BluetoothApi;
 
 public class MainActivity extends Activity {
 
